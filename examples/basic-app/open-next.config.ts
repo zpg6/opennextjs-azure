95f2e83a437865b7b70dff58afde5a78d015cd39
Zach Grimaldi <owner@grimaldi.tech>
2025-10-28 17:40:28 -0400

    feat: examples/basic-app

---
+++ examples/basic-app/open-next.config.ts
@@ -0,0 +1,28 @@
+// @ts-nocheck
+export default {
+    default: {
+        override: {
+            wrapper: () =>
+                import("./node_modules/opennext-azure/dist/adapters/wrappers/azure-functions.js").then(m => m.default),
+            converter: () =>
+                import("./node_modules/opennext-azure/dist/adapters/converters/azure-http.js").then(m => m.default),
+            incrementalCache: () =>
+                import("./node_modules/opennext-azure/dist/overrides/incrementalCache/azure-blob.js").then(
+                    m => new m.default()
+                ),
+            tagCache: () =>
+                import("./node_modules/opennext-azure/dist/overrides/tagCache/azure-table.js").then(
+                    m => new m.default()
+                ),
+            queue: () =>
+                import("./node_modules/opennext-azure/dist/overrides/queue/azure-queue.js").then(m => new m.default()),
+            proxyExternalRequest: "fetch",
+        },
+        routePreloadingBehavior: "none",
+    },
+    middleware: {
+        external: false,
+    },
+    buildOutputPath: ".",
+    appPath: ".",
+};
